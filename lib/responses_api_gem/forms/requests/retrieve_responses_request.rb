011c8a30f1f4e461aaf8596dfd6c745885be0715
lhoeg <larshoeg@gmail.com>
2018-08-13 14:09:45 +0200

    Fix since/until time format

--- lib/responses_api_gem/forms/requests/retrieve_responses_request.rb
+++ lib/responses_api_gem/forms/requests/retrieve_responses_request.rb
@@ -22,15 +22,15 @@ Hash.send :include, Hashie::Extensions
 
 module ResponsesApi
   class RetrieveResponsesRequest < FormRequest
-    ISO_8601_FORMAT = '%Y-%m-%dT%H:%M:%S.%L%z'.freeze
+    ISO_8601_FORMAT = '%Y-%m-%dT%H:%M:%S'.freeze
 
     def initialize(form_id, token: APIConfig.token, page_size: 25, page: nil, ts_since: nil, ts_until: nil, after: nil, before:nil, completed: true,
       sort: nil, query: nil, fields: nil)
       url = "#{APIConfig.api_request_url}/forms/#{form_id}/responses?"
       url << "page_size=#{page_size}&" unless page_size.nil?
       url << "page=#{page}&" unless page.nil?
-      url << "ts_since=#{ts_since.utc.strftime(ISO_8601_FORMAT)}&" unless ts_since.nil?
-      url << "ts_until=#{ts_until.utc.strftime(ISO_8601_FORMAT)}&" unless ts_until.nil?
+      url << "since=#{ts_since.utc.strftime(ISO_8601_FORMAT)}&" unless ts_since.nil?
+      url << "until=#{ts_until.utc.strftime(ISO_8601_FORMAT)}&" unless ts_until.nil?
       url << "after=#{after}&" unless after.nil?
       url << "before=#{before}&" unless before.nil?
       url << "sort=#{sort}&" unless sort.nil?
